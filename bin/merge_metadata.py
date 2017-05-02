5c48e903d72c406af0276e5b7ab18be03f9043f7
ravis100896 <ravi15165@iiitd.ac.in>
2017-05-02 12:55:53 +0530

    First Commit

---
+++ bin/merge_metadata.py
@@ -0,0 +1,4 @@
+#!/home/ravisharma/Desktop/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'pysaml2==4.0.5','merge_metadata.py'
+__requires__ = 'pysaml2==4.0.5'
+__import__('pkg_resources').run_script('pysaml2==4.0.5', 'merge_metadata.py')
---
+++ bin/parse_xsd2.py
@@ -0,0 +1,4 @@
+#!/home/ravisharma/Desktop/venv/bin/python
+# EASY-INSTALL-SCRIPT: 'pysaml2==4.0.5','parse_xsd2.py'
+__requires__ = 'pysaml2==4.0.5'
+__import__('pkg_resources').run_script('pysaml2==4.0.5', 'parse_xsd2.py')
